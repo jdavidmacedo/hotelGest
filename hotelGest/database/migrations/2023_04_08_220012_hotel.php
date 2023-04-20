abdec7aa742b423a996029cdd470ba2fb222046a
jdavidmacedo <85846966+jdavidmacedo@users.noreply.github.com>
2023-04-20 17:18:56 +0100

    name para nome - migrations

--- hotelGest/database/migrations/2023_04_08_220012_hotel.php
+++ hotelGest/database/migrations/2023_04_08_220012_hotel.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('hotel', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nome');
             $table->string('endereco');
             $table->string('cidade');
             $table->string('pais');
