4f7b31ea7e891668e36fa9040e29d09b967ed438
suila00 <100215878+suila00@users.noreply.github.com>
2023-04-24 17:07:34 +0100

    update

--- hotelGest/database/migrations/2023_04_24_142149_create_epoca_table.php
+++ hotelGest/database/migrations/2023_04_24_142149_create_epoca_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('epoca', function (Blueprint $table) {
+        Schema::create('epocas', function (Blueprint $table) {
             $table->id();
             $table->string('nome');
             $table->date('data_inicio');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('epoca');
+        Schema::dropIfExists('epocas');
     }
 };
---
+++ hotelGest/database/migrations/2023_04_24_155656_create_reserva_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reserva', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_cliente');
+            $table->unsignedBigInteger('id_hotel');
+            $table->unsignedBigInteger('id_quarto');
+            $table->date('data_checkin');
+            $table->date('data_checkout');
+            $table->enum('status', ['reservado', 'cancelado', 'concluido'])->default('reservado');
+            $table->timestamps();
+
+            $table->foreign('id_cliente')
+                ->references('id')
+                ->on('clientes')
+                ->onDelete('cascade');
+
+            $table->foreign('id_hotel')
+                ->references('id')
+                ->on('hoteis')
+                ->onDelete('cascade');
+
+            $table->foreign('id_quarto')
+                ->references('id')
+                ->on('quartos')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reserva');
+    }
+};
