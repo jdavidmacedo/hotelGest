8a4c2b2c4ad5d1ef7ad64c1dd5d272f5f200b561
jdavidmacedo <85846966+jdavidmacedo@users.noreply.github.com>
2023-07-11 13:21:54 +0100

    obter token

--- hotelGest/app/Http/Controllers/Auth/VerificationController.php
+++ hotelGest/app/Http/Controllers/Auth/VerificationController.php
@@ -35,6 +35,35 @@ class VerificationController extends Controller
      */
     public function __construct()
     {
+        $token;
+
+        $response = Http::withHeaders([
+            'Content-Type' => 'application/x-www-form-urlencoded',
+            'Cookie' => 'PHPSESSID=1db6128aa8451306a7343eeee8ad5844'
+        ])->post('https://devipvc.gesfaturacao.pt/gesfaturacao/server/webservices/api/mobile/v1.0.2/authentication', [
+            
+        'username' => 'ipvcweb2',
+        'password' => 'ipvcweb2'
+        ]);
+
+        if ($response->successful()) {
+            // O pedido de autenticação foi bem-sucedido
+            $responseData = $response->json();
+        
+            // Extrair o token de acesso da resposta
+            $accessToken = $responseData['_token'];
+        
+            // Armazenar o token de acesso na sessão
+            session(['access_token' => $accessToken]);
+
+         }else {
+            // O pedido de autenticação falhou
+            $errorCode = $response->status();
+            $errorMessage = $response->body();
+        
+            //dd($errorMessage);
+        }
+
         $this->middleware('auth');
         $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
