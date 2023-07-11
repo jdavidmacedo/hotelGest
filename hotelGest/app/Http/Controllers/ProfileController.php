35f173cbaf0d59f97828a9ad184816d3262a2380
jdavidmacedo <85846966+jdavidmacedo@users.noreply.github.com>
2023-07-11 13:26:11 +0100

    Update ProfileController.php

--- hotelGest/app/Http/Controllers/ProfileController.php
+++ hotelGest/app/Http/Controllers/ProfileController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Http;
 
 class ProfileController extends Controller
 {
